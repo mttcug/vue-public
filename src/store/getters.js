40cf62fac5933a8abb0b02b4b7d96f752f34b927
LIUMIN HE <heliumin222@gmail.com>
2019-10-22 07:36:48 +0800

    ff

---
+++ src/store/getters.js
@@ -0,0 +1,12 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  size: state => state.app.size,
+  device: state => state.app.device,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name,
+  introduction: state => state.user.introduction,
+  permission_routes: state => state.permission.routes,
+  roles: state => state.user.roles
+}
+export default getters
